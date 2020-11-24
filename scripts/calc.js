caeb96fbf25aa92420cb0daf135ce31186a8e750
LogisticBravo <jay.jbradley@gmail.com>
2020-11-24 00:10:14 +0000

    solve Drink about game

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,4 +1,4 @@
-Calculator = function() {
+/*Calculator = function() {
     this.value = 0;
 };
 
@@ -9,3 +9,20 @@ Calculator.prototype.add = function(number) {
         alert("Error!");
     }
 };
+*/
+
+/*Drink About Function
+function whatCanIDrink(age){
+    if(typeof(age) != 'number'){alert("Error - NaN");}
+    else if(age <=0){return "Sorry, I can’t tell what drink because that age is incorrect!";}
+    else if((age >0) && (age <= 14)){return "Drink Toddy";}
+    else if(age>=15 && age<=18){return "Drink Coke";}
+    else if(age>=19 && age <=21){return "Drink Beer";}
+    else if(age>=22 && age <=130){return "Drink Whisky"}
+    else{return "Sorry, I can’t tell what drink because that age is incorrect!"}}
+*/
+
+function fizzBuzz(numOne,numTwo){
+    if(numOne + numTwo === (numOne+numTwo)/3){return "FizzBuzz";}
+};
+
--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,4 +1,4 @@
-describe("Calculator", function(){
+/*describe("Calculator", function(){
 
     beforeEach(function(){
         calc = new Calculator;
@@ -23,5 +23,85 @@ describe("Calculator", function(){
             ("Error!");
             
         });
+    });
+}); 
+*/
+/*Drink About Test
+describe ("whatCanIDrink",function(){
+    describe ("Age Checks",function(){
+        it("should return Sorry, I can’t tell what drink because that age is incorrect!",function(){
+            expect(whatCanIDrink(0)).toBe("Sorry, I can’t tell what drink because that age is incorrect!");
+    });
+    it("should return Sorry, I can’t tell what drink because that age is incorrect! when called as whatCanIDrink(140)",function(){
+            var result = whatCanIDrink(140)
+            expect(result).toBe("Sorry, I can’t tell what drink because that age is incorrect!")
+        });
+});
+    describe ("less than 14",function(){
+        it("should return Drink Toddy",function(){
+            expect(whatCanIDrink(1)).toBe('Drink Toddy');
+        });
+    });
+    describe("less than 18", function(){
+        it("should return Drink Coke",function(){
+            expect(whatCanIDrink(15)).toBe('Drink Coke');
+        });
+    });
+    describe("less than 21", function(){
+        it("should return Drink Beer",function(){
+            expect(whatCanIDrink(20)).toBe('Drink Beer');
+        });
+    });
+    describe("less than 130", function(){
+        it("should return Drink Whisky",function(){
+            expect(whatCanIDrink(129)).toBe('Drink Whisky');
+        });
+    });
+    describe("Error Alert if not a number", function(){
+        it("should return Error - NaN",function(){
+            spyOn(window, "alert");
+            whatCanIDrink('anything but a number');
+            expect(window.alert).toHaveBeenCalledWith('Error - NaN');
+        });
+    });
+});
+*/
+/*SNIPPET FROM SOURCE CODE ANSWERS
+----------------------------------------------------------------
+describe("My whatCanIDrink function", function() {
+    // A test suite begins with a call to the global Jasmine function describe with two parameters: a string and a function.
+    beforeEach(function() {
+        drink = new whatCanIDrink();
+    });
+    
+    describe("Checks age", function() {
+        // Specs are defined by calling the global Jasmine function it
+        it("should exist", function() {
+            // Expectations are built with the function expect which takes a value, called the actual.
+            // Each matcher implements a boolean comparison between the actual value and the expected value.
+            // Any matcher can evaluate to a negative assertion by chaining the call to expect with a not before calling the matcher.
+            expect(whatCanIDrink).toBeDefined();
+        });
+        
+        it("should return drink toddy when called as whatCanIDrink(13)", function() {
+            var result = whatCanIDrink(13)
+            expect(result).toBe("Drink Toddy");
+        });
+-----------------------------------------------------------------
+*/
+
+describe('FizzBuzz Function',function(){
+    beforeEach(function(){
+        fizzBuzz = new fizzBuzz();
+    });
+    });
+    describe("checks number",function(){
+    it('should return FizzBuzz if the number is divisible by 3 and 5',function(){
+        var result = fizzBuzz(11,1)
+        expect(result).toBe("FizzBuzz");
+    });
+    it("should return Fizz if the number is divisible by 3",function(){
+        var result = fizzbuzz(6,6)
+        expect(result).toBe('Fizz');
     });
 });
